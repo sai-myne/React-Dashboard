625051b5ac73d3e8d26ffc74ef2bd43b54cfb9e3
sai-myne <snm.tgi@gmail.com>
2023-10-11 16:57:50 +0800

    Charts

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({ bgColor, color, size, text, borderRadius }) => {
+const Button = ({ icon, bgColor, color, size, text, borderRadius, click }) => {
   return (
-    <button type='button' style={{ backgroundColor: bgColor, color, borderRadius}} className={`text-${size} p-3 hover:drop-shadow-xl`}>
-      {text}
+    <button type='button' onClick={click} style={{ backgroundColor: bgColor, color, borderRadius}} className={`text-${size} p-3 hover:drop-shadow-xl`}>
+      {icon} {text}
     </button>
   )
 }
