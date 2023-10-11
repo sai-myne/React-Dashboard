f4c1d5bcf7966766b4761662c08ce9384207b177
sai-myne <snm.tgi@gmail.com>
2023-10-11 16:09:50 +0800

    Theme Settings

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext()
 
   const handleCloseSideBar = () => {
     if(activeMenu && screenSize <= 900) {
@@ -39,7 +39,7 @@ const Sidebar = () => {
                 {item.title}
               </p>
               {item.links.map(link => (
-                <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} className={({ isActive }) => isActive ? activeLink : normalLink }>
+                <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSideBar} style={({ isActive }) => ({backgroundColor: isActive ? currentColor : ''})} className={({ isActive }) => isActive ? activeLink : normalLink }>
                   {link.icon}
                   <span className="capitalize">
                     {link.name}
