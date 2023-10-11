d7e5df55ede34850f5dd7ec5700961deb3a57518
sai-myne <snm.tgi@gmail.com>
2023-10-11 14:26:15 +0800

    data table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
+
+import { employeesData, ContextMenu, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
+
 
 const Employees = () => {
   return (
-    <div>
-      Employees
+    
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Employee" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        toolbar={['Search']}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
     </div>
   )
 }
